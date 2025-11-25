2c63cbf477971fd40513177e0772ed30d5b2de53
ReyAllenAntonio <ReyAllenAntonio@SMU-0121>
2025-11-25 08:02:36 +0800

    rename and new u.i

--- Crud/Form1.Designer.vb
+++ Crud/Form1.Designer.vb
@@ -33,47 +33,51 @@ Partial Class Form1
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.MySqlData = New System.Windows.Forms.DataGridView()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
         CType(Me.MySqlData, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ButtonConnect
         '
         Me.ButtonConnect.Font = New System.Drawing.Font("Showcard Gothic", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.ButtonConnect.Location = New System.Drawing.Point(209, 33)
+        Me.ButtonConnect.Location = New System.Drawing.Point(116, 50)
         Me.ButtonConnect.Name = "ButtonConnect"
-        Me.ButtonConnect.Size = New System.Drawing.Size(308, 41)
+        Me.ButtonConnect.Size = New System.Drawing.Size(541, 41)
         Me.ButtonConnect.TabIndex = 0
         Me.ButtonConnect.Text = "Connect My SQL"
         Me.ButtonConnect.UseVisualStyleBackColor = True
         '
         'btnInsert
         '
-        Me.btnInsert.Location = New System.Drawing.Point(209, 235)
+        Me.btnInsert.Location = New System.Drawing.Point(116, 235)
         Me.btnInsert.Name = "btnInsert"
-        Me.btnInsert.Size = New System.Drawing.Size(308, 38)
+        Me.btnInsert.Size = New System.Drawing.Size(241, 38)
         Me.btnInsert.TabIndex = 1
         Me.btnInsert.Text = "CREATE (Insert Data)"
         Me.btnInsert.UseVisualStyleBackColor = True
         '
         'btnRead
         '
-        Me.btnRead.Location = New System.Drawing.Point(209, 279)
+        Me.btnRead.Location = New System.Drawing.Point(116, 279)
         Me.btnRead.Name = "btnRead"
-        Me.btnRead.Size = New System.Drawing.Size(308, 38)
+        Me.btnRead.Size = New System.Drawing.Size(241, 38)
         Me.btnRead.TabIndex = 2
         Me.btnRead.Text = "READ (Retrive Data)"
         Me.btnRead.UseVisualStyleBackColor = True
         '
         'txtName
         '
-        Me.txtName.Location = New System.Drawing.Point(294, 113)
+        Me.txtName.Location = New System.Drawing.Point(294, 148)
         Me.txtName.Name = "txtName"
         Me.txtName.Size = New System.Drawing.Size(223, 20)
         Me.txtName.TabIndex = 3
         '
         'txtAge
         '
-        Me.txtAge.Location = New System.Drawing.Point(294, 157)
+        Me.txtAge.Location = New System.Drawing.Point(294, 174)
         Me.txtAge.Name = "txtAge"
         Me.txtAge.Size = New System.Drawing.Size(223, 20)
         Me.txtAge.TabIndex = 4
@@ -97,7 +101,7 @@ Partial Class Form1
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(206, 116)
+        Me.Label2.Location = New System.Drawing.Point(206, 151)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(35, 13)
         Me.Label2.TabIndex = 7
@@ -106,7 +110,7 @@ Partial Class Form1
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(206, 160)
+        Me.Label3.Location = New System.Drawing.Point(206, 177)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(26, 13)
         Me.Label3.TabIndex = 8
@@ -129,11 +133,49 @@ Partial Class Form1
         Me.MySqlData.Size = New System.Drawing.Size(541, 127)
         Me.MySqlData.TabIndex = 10
         '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(416, 235)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(241, 38)
+        Me.btnDelete.TabIndex = 11
+        Me.btnDelete.Text = "DELETE"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(416, 279)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(241, 38)
+        Me.btnUpdate.TabIndex = 12
+        Me.btnUpdate.Text = "EDIT (update)"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'txtID
+        '
+        Me.txtID.Location = New System.Drawing.Point(294, 122)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(223, 20)
+        Me.txtID.TabIndex = 13
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(206, 125)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(16, 13)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "Id"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtID)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.btnDelete)
         Me.Controls.Add(Me.MySqlData)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
@@ -164,4 +206,8 @@ Partial Class Form1
     Friend WithEvents Label4 As Label
     Friend WithEvents MySqlData As DataGridView
     Friend WithEvents btnRead As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents txtID As TextBox
+    Friend WithEvents Label5 As Label
 End Class
--- Crud/Form1.vb
+++ Crud/Form1.vb
@@ -1,56 +1,110 @@
-﻿
-Imports MySql.Data.MySqlClient
+﻿Imports MySql.Data.MySqlClient
 
 Public Class Form1
 
-    Dim conn As MySqlConnection
-    Dim COMMAND As MySqlCommand
-    Private Sub ButtonConnect_Click(sender As Object, e As EventArgs) Handles ButtonConnect.Click
-        conn = New MySqlConnection
-        conn.ConnectionString = " server=localhost; userid=root; password=root; database= crud_demo_db"
+    Dim connectionString As String = "server=localhost; userid=root; password=root; database=crud_demo_db"
 
+
+    Public Sub LoadData()
         Try
-            conn.Open()
-            MessageBox.Show("Connected")
-
+            Using conn As New MySqlConnection(connectionString)
+                Dim query As String = "SELECT * FROM student_tbl"
+                Dim adapter As New MySqlDataAdapter(query, conn)
+                Dim table As New DataTable()
+                adapter.Fill(table)
+                MySqlData.DataSource = table
+            End Using
         Catch ex As Exception
-            MessageBox.Show(ex.Message)
-            conn.Close()
+            MsgBox(ex.Message)
         End Try
-
     End Sub
 
-    Private Sub ButtonInsert_Click(sender As Object, e As EventArgs) Handles btnInsert.Click
-        Dim query As String = "INSERT INTO student_tbl (name, age, email) VALUES (@name, @age ,@email)"
+
+    Private Sub btnInsert_Click(sender As Object, e As EventArgs) Handles btnInsert.Click
+
+        Dim query As String = "INSERT INTO student_tbl (name, age, email) 
+                               VALUES (@name, @age, @email)"
+
         Try
-            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database= crud_demo_db")
+            Using conn As New MySqlConnection(connectionString)
                 conn.Open()
                 Using cmd As New MySqlCommand(query, conn)
                     cmd.Parameters.AddWithValue("@name", txtName.Text)
                     cmd.Parameters.AddWithValue("@age", txtAge.Text)
                     cmd.Parameters.AddWithValue("@email", txtEmail.Text)
                     cmd.ExecuteNonQuery()
-                    MessageBox.Show(" Record Insert Succesfully ")
                 End Using
             End Using
-        Catch ex As Exception
 
+            MessageBox.Show("Record Inserted Successfully")
+            LoadData()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
         End Try
 
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnRead.Click
-        Dim query As String = "SELECT * FROM crud_demo_db.student_tbl;"
+    Private Sub btnRead_Click(sender As Object, e As EventArgs) Handles btnRead.Click
+        LoadData()
+    End Sub
+
+    Private Sub dgvStudents_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles MySqlData.CellClick
+        If e.RowIndex >= 0 Then
+            Dim row As DataGridViewRow = MySqlData.Rows(e.RowIndex)
+            txtID.Text = row.Cells("id").Value.ToString()
+            txtName.Text = row.Cells("name").Value.ToString()
+            txtAge.Text = row.Cells("age").Value.ToString()
+            txtEmail.Text = row.Cells("email").Value.ToString()
+        End If
+    End Sub
+
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        Dim query As String = "UPDATE student_tbl 
+                               SET name=@name, age=@age, email=@email 
+                               WHERE id=@id"
+
         Try
-            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database= crud_demo_db")
-                Dim adapter As New MySqlDataAdapter(query, conn)
-                Dim table As New DataTable()
-                adapter.Fill(table) 'FROM
-                MySqlData.DataSource = table 'Display to DATAGRIDVIEW
+            Using conn As New MySqlConnection(connectionString)
+                conn.Open()
+                Using cmd As New MySqlCommand(query, conn)
+                    cmd.Parameters.AddWithValue("@id", txtID.Text)
+                    cmd.Parameters.AddWithValue("@name", txtName.Text)
+                    cmd.Parameters.AddWithValue("@age", txtAge.Text)
+                    cmd.Parameters.AddWithValue("@email", txtEmail.Text)
+                    cmd.ExecuteNonQuery()
+                End Using
             End Using
+
+            MessageBox.Show("Record Updated Successfully")
+            LoadData()
+
         Catch ex As Exception
             MsgBox(ex.Message)
         End Try
     End Sub
-End Class
 
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+
+        Dim query As String = "DELETE FROM student_tbl WHERE id=@id"
+
+        Try
+            Using conn As New MySqlConnection(connectionString)
+                conn.Open()
+                Using cmd As New MySqlCommand(query, conn)
+                    cmd.Parameters.AddWithValue("@id", txtID.Text)
+                    cmd.ExecuteNonQuery()
+                End Using
+            End Using
+
+            MessageBox.Show("Record Deleted Successfully")
+            LoadData()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+End Class
